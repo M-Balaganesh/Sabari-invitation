0de9786c2d9023f7a563cb4ccc02d08b0aecb0d8
M.Balaganesh <balag8563@gmail.com>
2024-09-06 17:08:56 +0530

    Update countdown-custom.js

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2024, 10, 19, 8)}); // year, month, date, hour
+            jQuery('#defaultCountdown').countdown({until: new Date(2025, 10, 19, 8)}); // year, month, date, hour
         });
 });		
 
